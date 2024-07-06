48c4f2284d2f856c703359f81db01a8c365c1197
Vanessa Morales <auxiliadoramoralesscj@gmail.com>
2024-07-05 19:15:43 -0600

    se corrigieron los errorres de la validación del formulario y se agregó el footer

--- main.js
+++ main.js
@@ -2,79 +2,71 @@ import menuHamburguer from "./js/hamburguer.js";
 import { cards_container } from "./js/habilidades.js";
 import { proyecto_container } from "./js/proyectos.js";
 
-
-
 const form = document.getElementById("form");
+
 const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
   e.preventDefault();
-  validateInputs();
+  checkInputs();
 });
 
-const setError = (element, message) => {
-  const errorDisplay = document.getElementById(`error-${element.id}`);
-  errorDisplay.textContent = message;
-  element.classList.add("error");
-  element.classList.remove("success");
-};
-
-const setSuccess = (element) => {
-  const errorDisplay = document.getElementById(`error-${element.id}`);
-  errorDisplay.innerText = " ";
-  element.classList.add("success");
-  element.classList.remove("error");
-};
-
-const isValidEmail = (email) => {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
-
-const validateInputs = () => {
-  const nombreValue = nombre.value.trim();
+function checkInputs(){
+  const nombreValue= nombre.value.trim();
   const emailValue = email.value.trim();
-  const asuntoValue = asunto.value.trim();
-  const mensajeValue = mensaje.value.trim();
+  const asuntoValue =asunto.value.trim();
+  const mensajeValue= mensaje.value.trim();
 
-  if (nombreValue === "") {
-    setError(nombre, "El campo nombre está vacío");
-  } else if (nombreValue.length > 50) {
-    setError(nombre, "El nombre no debe tener más de 50 caracteres");
-  } else {
-    setSuccess(nombre);
+  if(nombreValue=== ""){
+    setErrorFor(nombre," No dejar el campo nombre vacio");
+  }else if (nombreValue.length>50){
+    console.log(nombre.value);
+    setErrorFor(nombre, "Debe contener como máximo 50 caracteres");
+  }else{
+    setSuccessFor(nombre);
   }
 
-  if (emailValue === "") {
-    setError(email, "El campo email está vacío");
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, "Ingrese un email válido");
-  } else {
-    setSuccess(email);
+  if(emailValue===""){
+    setErrorFor(email,"No dejar el campo email vacio");
+  }else if(!isEmail(emailValue)){
+    setErrorFor(email, "Ingrese un email válido");
+  }else{
+    setSuccessFor(email);
   }
 
-  if (asuntoValue === "") {
-    setError(asunto, "El campo asunto está vacío");
-  } else if (asuntoValue.length > 50) {
-    setError(asunto, "El asunto no debe tener más de 50 caracteres");
-  } else {
-    setSuccess(asunto);
+  if(asuntoValue===""){
+    setErrorFor(asunto, "No dejar el campo asunto vacío");
+  }else if(asuntoValue.length>50){
+    setErrorFor(asunto, "Debe contener como máximo 50 caracteres");
+  }else{
+    setSuccessFor(asunto);
   }
 
-  if (mensajeValue === "") {
-    setError(mensaje, "El campo mensaje está vacío");
-  } else if (mensajeValue.length > 300) {
-    setError(mensaje, "El mensaje no debe tener más de 300 caracteres");
-  } else {
-    setSuccess(mensaje);
+  if(mensajeValue===""){
+    setErrorFor(mensaje, "No dejar el campo mensaje vacío");
+  }else if(mensajeValue.length>50){
+    setErrorFor(mensaje, "Debe contener como máximo 300 caracteres");
+  }else{
+    setSuccessFor(mensaje);
   }
-};
 
-document.addEventListener("DOMContentLoaded", () => {
-  menuHamburguer();
-  cards_container();
-  proyecto_container();
-});
+}
+
+function setErrorFor(input, mensaje){
+  const formControl =input.parentElement;
+  const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = mensaje;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
